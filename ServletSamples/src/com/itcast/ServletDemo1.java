3ad86425bd673b39fca7a827fb953bd8d213a63e
Jing2012 <2603852892@qq.com>
2017-12-05 22:18:36 +0800

    ServletSamples

---
+++ ServletSamples/src/com/itcast/ServletDemo1.java
@@ -0,0 +1,23 @@
+package com.itcast;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by jingjing.hu on 2017/12/5.
+ */
+public class ServletDemo1 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+        writer.write("<h1>Hello First Servlet!</h1>");
+    }
+}
