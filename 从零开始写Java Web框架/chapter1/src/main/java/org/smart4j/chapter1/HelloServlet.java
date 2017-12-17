843dadbbc9c99914fd4118c61cf987863f4075cd
Jing2012 <2603852892@qq.com>
2017-12-17 21:37:13 +0800

    从零开始写Java Web框架----chapter1

---
+++ 从零开始写Java Web框架/chapter1/src/main/java/org/smart4j/chapter1/HelloServlet.java
@@ -0,0 +1,31 @@
+package org.smart4j.chapter1;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by jingjing.hu on 2017/12/17.
+ */
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String currTime = dateFormat.format(new Date());
+
+        request.setAttribute("currTime", currTime);
+        request.getRequestDispatcher("/jsp/hello.jsp").forward(request, response);//请求转发到hello.jsp
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+}
