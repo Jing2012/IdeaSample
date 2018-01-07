5acb1f810051bb17fb63c03f3a35a2a9b01cd5e0
Jing2012 <2603852892@qq.com>
2018-01-08 07:17:33 +0800

    chapter2

---
+++ 从零开始写Java Web框架/chapter2/src/main/java/org/smart4j/chapter2/service/CustomerService.java
@@ -0,0 +1,38 @@
+package org.smart4j.chapter2.service;
+
+import org.smart4j.chapter2.model.Customer;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by jingjing.hu on 2018/1/7.
+ */
+public class CustomerService {
+
+    public List<Customer> getCustomerList() {
+        //TODO
+        return null;
+    }
+
+    public Customer getCustomer(long id) {
+        //TODO
+        return null;
+    }
+
+    public boolean createCustomer(Map<String, Object> fieldMap) {
+        //TODO
+        return false;
+    }
+
+    public boolean updateCustomer(long id, Map<String, Object> fieldMap) {
+        //TODO
+        return false;
+    }
+
+    public boolean deleteCustomer(long id) {
+        //TODO
+        return false;
+    }
+
+}
